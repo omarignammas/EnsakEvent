11aaf61c8f3b02d25c8fd164456aba1902675d3d
Abdelhamid-0p <abdelhamid.hachimi-alaoui@uit.ac.ma>
2024-01-14 02:10:06 +0100

    Frontendorg abde 1

---
+++ login.php
@@ -0,0 +1,138 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Ensakenitra_Events";
+
+$link=mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$link) {
+    die("Échec de la connexion à la base de données : " . mysqli_connect_error($link));
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['sub'])) {
+    
+    $_SESSION['login']=$_POST['email'];
+    $_SESSION['password']=$_POST['password'];
+    $login = mysqli_real_escape_string($link, $_POST['email']);
+    $password = mysqli_real_escape_string($link, $_POST['password']);
+
+    $sql = "SELECT * FROM `organisateur` WHERE Mail_Org='$login';";
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $data = mysqli_fetch_assoc($result);
+        if ($data && $password = $data['pass']) {
+            $_SESSION['loggedin'] = true;
+            $_SESSION['login'] = $login;
+            $_SESSION['pass'] = $pass;
+            $_SESSION['nom_org'] = $data['nom_org'];
+            $_SESSION['CIN_resp'] = $data['CIN_resp'];
+            header('location: pages\organisateur\dem_event\form-event.php');
+            exit;
+        } else {
+            
+            echo "<h2>Votre login ou password est incorrect</h2>";
+        }
+    } else {
+        die('Error: ' . mysqli_error($link));
+    }
+}
+
+mysqli_close($link);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: whitesmoke;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        form {
+            background-color: #fff;
+            padding: 40px;
+            border-radius: 8px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+            width: 400px;
+            text-align: center;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+            font-weight: bold;
+            color: #333;
+            font-size: large;
+            font-family: serif;
+        }
+
+        input {
+            width: calc(100% - 20px);
+            padding: 10px;
+            margin-bottom: 20px;
+            box-sizing: border-box;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            font-size: 16px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            font-family: serif;
+            font-size: large;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+
+        a {
+            color: #3498db;
+            text-decoration: none;
+            font-weight: bold;
+            font-size: 15px;
+        }
+        p {
+            font-family:serif;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+        h2{
+            font-family: serif;
+        }
+    </style>
+</head>
+<body>
+    <form action="" method="post">
+        <h2>INSCRIPTION D'ORGANISATION</h2>
+        <label for="email">Email:</label>
+        <input type="text" name="email" placeholder="Enter your login">
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" placeholder="Enter your password">
+
+        <input  href="Espace organisation.php" style="margin-bottom: 10px;" type="submit" name="sub" value="Connexion">
+        <input style="margin-top: 0px;" type="submit" name="sub" value="Retour à l'accueil">
+
+        <p>Don't have an account? <a href="pages\organisateur\dem_org\form_org.html">Demande d'organisation</a></p>
+    </form>
+</body>
+</html>
---
+++ pages/organisateur/dem_event/form-prom.php
@@ -0,0 +1,56 @@
+<?php
+include ("../../../includes/connexion.php");
+if($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST['submit'])){
+// Récupérer les données du formulaire
+$nom_org =  $_POST['nom_org'];
+$Email =$_POST['email_org'];
+$description =$_POST['description'];
+$titre = $_POST['titre'];
+$type = $_POST['type'];
+$gsm = $_POST['gsm'];
+$datedebut = $_POST['datedebut'];
+$datefin = $_POST['datefin'];
+$deadline = $_POST['deadline'];
+$detail = $_POST['detail'];
+$lieu = $_POST['lieu'];
+
+// Télécharger l'image
+if(isset($_FILES['fichier']) and $_FILES['fichier']['error']==0){
+    $dossier='../uploads/event';
+    $temp_name=$_FILES['fichier']['tmp_name'];
+    if(!is_uploaded_file($temp_name)){
+        exit("le fichier est introuvable!");
+    }
+if($_FILES['fichier']['size']>=1000000){
+     exit("Erreur,fichier stockage ");
+}
+$infosfichier=pathinfo($_FILES['fichier']['name']);
+$extension_upload=$infosfichier['extension'];
+
+$extension_upload=strtolower($extension_upload);
+$extension_autorities=array('png','jpeg','jpg');
+
+if(!in_array($extension_upload,$extension_autorities)){
+    exit("Erreur,Veuillez inserer une image svp (extentions autorisee png)");
+}
+$nom_photo=$nom_org.".".$extension_upload;
+if(!move_uploaded_file($temp_name,$dossier.$nom_photo)){
+    exit("<h2>Probleme dans telechargement de l'image,Ressayer!</h2>");
+}
+
+}
+// Insérer les données dans la base de données
+    $sql = "INSERT INTO event (mail,nom_org,titre,descp, type, debut, fin, local, img, deadline,detail, gsm,)
+        VALUES ('$Email','$nom_org','$titre','$description', '$type', '$datedebut', '$datefin','$lieu','$nom_photo','$deadline', '$detail', '$gsm')";
+
+if ($result = mysqli_query($conn, $sql)) {
+    $row= mysqli_fetch_assoc($result);
+    $_SESSION['id_event']=$row['id_event'];
+    header('location:..\dem_event\form-event.php');
+} else {
+    echo "Erreur : " . $sql . "<br>" . $conn->error;
+}
+}    
+
+$conn->close();
+?>
